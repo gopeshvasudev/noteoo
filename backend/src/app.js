fb71c7a5d92d14459963fdb764190d2c5aba0c54
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2025-02-04 22:13:56 +0530

    created a server using express

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectToDatabase } from "./config/database.connection.js";
+import authRouter from "./routes/auth.routes.js";
+config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+    methods: "GET, POST, PATCH",
+  })
+);
+
+//Routes
+app.use("/api/v1/auth/", authRouter);
+
+app.listen(PORT, () => {
+  connectToDatabase();
+  console.log(`Server is running at port: ${PORT}`);
+});
