bc25e24bea53cec0256a2795e674a642ae6749a3
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2025-02-04 22:18:50 +0530

    created signin api

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -61,4 +61,45 @@ const signupHandler = async (req, res) => {
   }
 };
 
-export { signupHandler};
+const signinHandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //Checking the user is exists or not.
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      throw new HttpError(400, "Invalid credentials");
+    }
+
+    //Checking the password is correct or not.
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw new HttpError(400, "Invalid credentials");
+    }
+
+    //If everything goes well then creating a access and refresh token and setting it into cookie.
+    const accessToken = createAccessToken({ _id: user._id });
+    const refreshToken = createRefreshToken({ _id: user._id });
+
+    return res
+      .status(200)
+      .cookie("token", refreshToken, {
+        maxAge: 1000 * 60 * 60 * 24 * 15,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+      })
+      .json({
+        success: true,
+        message: `Welcome back, ${user.username}!`,
+        accessToken,
+      });
+  } catch (error) {
+    console.error("Signin user error: " + error.message);
+    errorHandler(res, error.statusCode, error.message);
+  }
+};
+
+export { signupHandler, signinHandler };
