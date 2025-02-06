bbd3091f4dcc36ee9561632ec8fd8ef0632ae891
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2025-02-06 11:34:49 +0530

    Applied routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+
+const Home = lazy(() => import("./pages/Home.jsx"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
